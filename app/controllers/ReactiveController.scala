a5129476ba01225b73f6b5e6bb2d03b88ec8a550
Zahir Abdi <megaz1989@gmail.com>
2014-01-07 23:25:29 +0000

    Adding license on source code

--- app/controllers/ReactiveController.scala
+++ app/controllers/ReactiveController.scala
@@ -2,32 +2,46 @@ package controllers
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
-import scala.util.Failure
-
 import play.api.libs.json.Format
 import play.api.libs.json.JsObject
 import play.api.libs.json.Json
 import play.api.libs.json.Json.toJsFieldJsValueWrapper
+import play.api.mvc.SimpleResult
 import play.autosource.reactivemongo.ReactiveMongoAutoSourceController
 import reactivemongo.api.Cursor
+import play.api.libs.json.JsArray
+import scala.util.Failure
 
+/**
+  * Copyright 2013 Zahir Abdi (@megaz)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
 abstract class ReactiveController[T](implicit ctx: ExecutionContext, format: Format[T]) extends ReactiveMongoAutoSourceController[T] {
 
-  def find(key: String, value: String) = Future {
-    Async { 
-      val cursor: Cursor[JsObject] = coll.
-        find(Json.obj(key -> value)).
-        cursor[JsObject]
+  def find(key: String, value: String): Future[SimpleResult] = {
 
-      val futureUsersList: Future[List[JsObject]] = cursor.toList()
+    val cursor: Cursor[JsObject] = coll.
+      find(Json.obj(key -> value)).
+      cursor[JsObject]
 
-      futureUsersList onComplete {
-        case Failure(t) => Ok("An error has occured: " + t.getMessage)
-      }
-      futureUsersList.map { users =>
-        Ok(Json.toJson(users))
-      }
+    val futureList: Future[List[JsObject]] = cursor.collect[List]()
+
+    futureList onFailure {
+      case t => InternalServerError("An error has occured using find: " + t.getMessage)
     }
-  }
-      
-} 
+
+    futureList.map { item => Ok(Json.toJson(item)) }
+  } 
+  
+}
--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -1,26 +1,40 @@
 package controllers
 
 import scala.concurrent.ExecutionContext.Implicits.global
-
 import play.api.libs.json.JsObject
 import play.api.mvc.Action
 import play.modules.reactivemongo.json.collection.JSONCollection
+import play.api.libs.json.Json
+import models.User
+import play.api.Logger
 
-
-
-object Users extends ReactiveController[JsObject] {
-    
+/**
+  * Copyright 2013 Zahir Abdi (@megaz)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+object Users extends ReactiveController[User] {
+     
   lazy val coll = db.collection[JSONCollection]("users")
   
-  def findByUsername(username: String) = Action {
-    Async {
-      find("username", username)
-    } 
+  def findByUsername(username: String) = Action.async { request =>
+    Logger.info("findByUsername");
+    find("username", username) 
   }
 
-  def findByEmail(email: String) = Action {
-    Async {
-      find("email", email)
-    }
+  def findByEmail(email: String) = Action.async { request =>
+    Logger.info("findByEmail");
+    find("email", email)
   }
+  
 }
