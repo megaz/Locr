bb1a33b7cd738276fafc70fbac80582cea5eafb6
Zahir Abdi <megaz1989@gmail.com>
2014-01-09 20:56:09 +0000

    Adding User Model

---
+++ app/models/User.scala
@@ -0,0 +1,26 @@
+package models
+
+import play.api.libs.json.Json
+import org.joda.time.DateTime
+
+/**
+  * Copyright 2013 Zahir Abdi (@megaz)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+case class User(username: String, password: String, email: String, name: Option[String], phone : Option[String], registerDate : DateTime)
+
+object User{
+  implicit val fmt = Json.format[User]
+}
+
